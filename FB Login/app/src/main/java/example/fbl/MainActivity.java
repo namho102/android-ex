60f8fd83c8293c966a259dc43303d15fe913528f
Nam <namhoang.td09@gmail.com>
2017-09-05 20:40:22 +0700

    Initial commit :sparkles:

---
+++ FB Login/app/src/main/java/example/fbl/MainActivity.java
@@ -0,0 +1,21 @@
+package example.fbl;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
